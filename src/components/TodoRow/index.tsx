d14f6cf4d014154a7608bd64e9aa7a7d6005c746
fukui-takanori <takanori.fukui@drecom.co.jp>
2024-09-03 21:44:28 +0900

    add: 実施予定日を追加できるようにする

--- src/components/TodoRow/index.tsx
+++ src/components/TodoRow/index.tsx
@@ -1,9 +1,11 @@
 import type { Schema } from '@/amplify/data/resource'
+import { Calendar } from '@/src/svgs/Calendar'
 import { CheckIcon } from '@/src/svgs/CheckIcon'
 import { PencilSquare } from '@/src/svgs/PencilSquare'
 import { Trash } from '@/src/svgs/Trash'
 import { generateClient } from 'aws-amplify/api'
 import { useState } from 'react'
+import { formatDate } from './formatDate'
 
 const client = generateClient<Schema>()
 
@@ -11,8 +13,10 @@ type Props = {
   todo: Schema['Todo']['type']
 }
 export const TodoRow = ({ todo }: Props) => {
-  const [isEdit, setIsEdit] = useState(false)
+  const [isEditContent, setIsEditContent] = useState(false)
+  const [isEditExecutionDate, setIsEditExecutionDate] = useState(false)
   const [tmpEditingTodo, setTmpEditingTodo] = useState(todo.content ?? '')
+  const [tmpEditingExecutionDate, setTmpEditingExecution] = useState(todo.executionDate ?? formatDate(new Date()))
   const toggleDone = async () => {
     const { errors } = await client.models.Todo.update({
       id: todo.id,
@@ -24,20 +28,36 @@ export const TodoRow = ({ todo }: Props) => {
     }
   }
 
-  const startEdit = () => {
-    setIsEdit(true)
+  const startEditContent = () => {
+    setIsEditContent(true)
   }
 
-  const editTodo = async () => {
+  const editContent = async () => {
     const { errors } = await client.models.Todo.update({
       id: todo.id,
       content: tmpEditingTodo,
     })
-    setIsEdit(false)
+    setIsEditContent(false)
 
     if (errors) {
       console.error(errors)
     }
+  }
+
+  const editExecutionDate = async () => {
+    const { errors } = await client.models.Todo.update({
+      id: todo.id,
+      executionDate: tmpEditingExecutionDate,
+    })
+    setIsEditExecutionDate(false)
+
+    if (errors) {
+      console.error(errors)
+    }
+  }
+
+  const startEditExecutionDate = () => {
+    setIsEditExecutionDate(true)
   }
 
   const remove = async () => {
@@ -55,17 +75,22 @@ export const TodoRow = ({ todo }: Props) => {
       <button className="size-4 border" onClick={toggleDone}>
         {todo.isDone && <CheckIcon />}
       </button>
-      {isEdit ? (
+      {isEditContent && (
         <input
           value={tmpEditingTodo}
           onChange={(e) => setTmpEditingTodo(e.target.value)}
-          onBlur={editTodo}
+          onBlur={editContent}
         />
-      ) : (
+      )}
+      {isEditExecutionDate && <input type="date" value={tmpEditingExecutionDate} onChange={e => setTmpEditingExecution(e.target.value)} onBlur={editExecutionDate} />}
+      {!isEditContent && !isEditExecutionDate && (
         <>
-          {todo.content}
-          <button onClick={startEdit}>
+          {todo.content}-{todo.executionDate}
+          <button onClick={startEditContent}>
             <PencilSquare className="size-4" />
+          </button>
+          <button onClick={startEditExecutionDate}>
+            <Calendar className="size-4" />
           </button>
           <button onClick={remove}>
             <Trash className="size-4" />
